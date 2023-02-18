f3b5f346ebae0bf67cb0511e095ad8247529c40d
zeeshan <through.learning@gmail.com>
2023-02-18 17:24:23 +0530

    Final counter example calling parent methods from props

--- src/components/Counter/CounterButton.jsx
+++ src/components/Counter/CounterButton.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 export default function CounterButton(props) {
 
-    
-
-    function incrementCounterFunction() {
-        props.methods.incr(props.value);
-    }
-
-    function decrementCounterFunction() {
-        props.methods.decr(props.value);
-    }
-
     return (
         <div className='Counter'>
             <button className='counterButton'
-                onClick={decrementCounterFunction}
+                onClick={() => props.methods.decr(props.value)}
             >-{props.value}</button>
             <button className='counterButton'
-                onClick={incrementCounterFunction}
+                onClick={() => props.methods.incr(props.value)}
             >+{props.value}</button>
         </div>
     )
